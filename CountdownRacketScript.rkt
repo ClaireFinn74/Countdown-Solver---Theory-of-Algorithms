dfed696c71d487386c73e01fe1e06cfbc1d01969
[ClaireFinn] <[clairefinn74@yahoo.ie]>
2017-02-18 16:45:46 +0000

    [Target Number randomly generated]

--- CountdownRacketScript.rkt
+++ CountdownRacketScript.rkt
@@ -6,3 +6,6 @@
 ;Defining a list can be done as follows:
 
 (define RandomList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+;This will be my Target number between 101 and 999 generated randomly
+(random 101 999)

908ba5e64239e02a5407b9e03a822a2a8b2eb595
[ClaireFinn] <[clairefinn74@yahoo.ie]>
2017-02-18 16:22:34 +0000

    [This commit defines a list of numbers called RandomList with a short description]

---
+++ CountdownRacketScript.rkt
@@ -0,0 +1,6 @@
+#lang racket
+; I will start by defining my lists for countdown
+; My Random list of numbers and my Target Number between the numbers 100-199 inclusive.
+;Defining a list can be done as follows:
+
+(define RandomList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))

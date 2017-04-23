0a8021b22c58f434c86b4411bce465ce182f8e2c
Claire Finn <Clairefinn74@yahoo.ie>
2017-04-23 16:05:16 +0100

    Providing my reader with some of my functions in my CountDown Script

--- Countdown-Solver---Theory-of-Algorithms.rkt
+++ Countdown-Solver---Theory-of-Algorithms.rkt
@@ -50,7 +50,7 @@
 (define RandomListFifthNumber (list-ref(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100) (random 24)))
 (define RandomListSixthNumber (list-ref(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100) (random 24)))
 
-'RandomListFirstNumber: RandomListFirstNumber
+'RandomListFirstNumber: RandomListFirstNumber '(This is the firstnumber)
 'RandomListSecondNumber: RandomListSecondNumber
 'RandomListThirdNumber: RandomListThirdNumber
 'RandomListFourthNumber: RandomListFourthNumber
@@ -64,12 +64,12 @@
  'TargetNumber: TargetNumber
 
 #| I'm defining a simple function called Sum bound to RandomSixAdd to add the result of all of the Random Six numbers together.
- Checking for nulls in the first part of the if statement and if not null then add the car of the list to the cdr of the list recursively.
-|#
+ Checking for nulls in the first part of the if statement and if not null then add the car of the list to the cdr of the list recursively. |#
+
 (define(sum RandomSixAdd)
 (if(null? RandomSixAdd)
    0
-   (+ (car RandomSixAdd) (sum(cdr RandomSixAdd)))))
+   (+ (car RandomSixAdd) (sum(cdr RandomSixAdd))))) 
 
 #| The function sum is now defined to recurse through a list up above and the RandomListFirstNumber...RandomListSixthNumber lists
    already pick out a random number each to display to the output window.
@@ -83,65 +83,66 @@
 sumPrint
 
 ; I now do the same for multiply. Note that I now use 1 instead of 0 as you cannot multiply by 0.
-(define(mult RandomSixMultiply)
+#| (define(mult RandomSixMultiply)
 (if(null? RandomSixMultiply)
    1
    (* (car RandomSixMultiply) (mult(cdr RandomSixMultiply)))))
 
 'MultiplyRandomSix: (define multPrint(mult(list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber)))
+|#
 
 ;Print out the result of the multilication
-multPrint
+;multPrint
 
 #| I'm now doing the same for subtract, although, this output will now start to produce negative numbers which I don't want.
    I think I have a mechanism for dealing with negative numbers below, by printing an error message if negative.
 |#
 
-(define(subt RandomSixSubtract)
+#|(define(subt RandomSixSubtract)
 (if(null? RandomSixSubtract)
    0
    (- (car RandomSixSubtract) (subt(cdr RandomSixSubtract)))))
-
+|#
 #| I define a function called subtneg which prints true(#t) or false (#f) if the result of subt is negative.
    Now seeing #t or #f wouldn't really make much sense to a user so I chose not to use this function, instead I use the function below called subtPrint.
    Keeping subtneg incase I do need it at some stage though.
 |#
 
-(define subtneg(negative? (subt(list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber))))
+;(define subtneg(negative? (subt(list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber))))
 
 
 #| Using a function defined as subtPrint is where I deal with negative numbers. subtPrint is just doing the same thing as the subtraction
    of the six random numbers recursively would do but the key thing is that I'm giving this function itself a name, which I didn't do
    previously with  sum and mult. Naming the function allows me to use it later on to deal with negative numbers. 
 |#
- (define subtPrint(subt(list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber)))
+ ;(define subtPrint(subt(list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber)))
 
 ;Print out the result of the subtraction
-'SubtractRandomSix: subtPrint
+;'SubtractRandomSix: subtPrint
 
 #|  Now, using subtPrint defined previously, I check if subtPrint is less than (<) 0, print the result.
     Else, Print a message saying "Negative Numbers are not allowed!"
 |#
 
-         (define subtPrintHandler(if(< 0 subtPrint)
+#|         (define subtPrintHandler(if(< 0 subtPrint)
              subtPrint
              "Negative Numbers not allowed!"))
-
+|#
 
 ;Print out the result of the subtraction handler for negative numbers
-'subtPrintHandler: subtPrintHandler
+;'subtPrintHandler: subtPrintHandler
 
 ; Now onto divide, which will also start causing problems as it produces fractions and I want whole numbers to be output.
-(define(div RandomSixDivide)
+#|(define(div RandomSixDivide)
 (if(null? RandomSixDivide)
    1
-   (/ (car RandomSixDivide) (div(cdr RandomSixDivide)))))
+|# ;  (/ (car RandomSixDivide) (div(cdr RandomSixDivide)))))
 
 ;To deal with fractions I use the 'round' function to round the fraction up to the nearest whole number which will be 0 lots of the time.
-'DivideRandomSix: (define divPrint (div(list(round(/ RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber)))))
+;'DivideRandomSix: (define divPrint (div(list(round(/ RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber)))))
 
 ;This prints out the division to the output
-divPrint
+;divPrint
 
 #| Now that I have the six random numbers and the target number set up, sum,mult,subt,div functions and a way to deal with fractions while dividing and
    negative numbers while subtracting, I need to get to the most important part: Getting all possible permutations of the list (1-10, 25, 50, 75, 100)
@@ -155,27 +156,27 @@ divPrint
 |#
 
 ;Putting the six random numbers into a list
-(define RandomSixAll (list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber))
+;(define RandomSixAll (list RandomListFirstNumber RandomListSecondNumber RandomListThirdNumber RandomListFourthNumber RandomListFifthNumber RandomListSixthNumber))
 ;Now I'm defining a list of operands to use:
-(define operandsList(list '+ '- '/ '*))
+;(define operandsList(list '+ '- '/ '*))
 
 ;Printing RandomSixAll
-'RandomSixAll RandomSixAll
+;'RandomSixAll RandomSixAll
 ;Printing the operandsList
-'operandsList: operandsList
+;'operandsList: operandsList
 
 ;Now I can try to find the cartesian-product (as described above) of my 5 operands.
 ;I am using 5 operands as the Random Six Numbers will need to have an operand between them at all times.
-(define operandsForSixNumbers (cartesian-product operandsList operandsList operandsList operandsList operandsList))
+;(define operandsForSixNumbers (cartesian-product operandsList operandsList operandsList operandsList operandsList))
 
 ;Printing operandsForSixNumbers
 ;'operandsForSixNumbers operandsForSixNumbers
 
 ;Now I'm getting all of the permutations of my random six numbers list
-(define rsaPerm(remove-duplicates(permutations RandomSixAll)))
+;(define rsaPerm(remove-duplicates(permutations RandomSixAll)))
 
 ;Now I'm putting the operators and numbers together, removing duplicates for optimization
-(define numsAndOps(remove-duplicates (cartesian-product  rsaPerm  operandsForSixNumbers)))
+;(define numsAndOps(remove-duplicates (cartesian-product  rsaPerm  operandsForSixNumbers)))
 
 ;printing out he numbers and operators
 ;'numsAndOps numsAndOps
@@ -188,7 +189,7 @@ divPrint
 (define (pop-stack!)
 ;Now I will define 'arg', as the 'first' element in the stack/the car of the stack
   (define arg (first stack))
-;I'm now 'set'ting the stack as the 'rest' of the stack/car of the stack, without the first element/car
+;I'm now 'set'ting the stack equal to the 'rest' of the stack/car of the stack, without the first element/car
   (set! stack (rest stack))
   arg)
 
@@ -206,14 +207,33 @@ divPrint
     [(number? arg) (push-stack! arg)]
 ;Or, is the argument being passed within the reader equal to a * + / or - operand?
     [(or (equal? * arg) (equal? + arg) (equal? / arg) (equal? - arg))
-;define 'op-result', whereby, two arguments already on the stack ar popped off and evaluated with the operand passed
+;define 'op-result', whereby, two arguments already on the stack are popped off and evaluated with the operand passed
      (define op-result (arg (pop-stack!) (pop-stack!)))
 ;push the result of the evaluation of the two elements popped off with the operand onto the stack
      (push-stack! op-result)]))
 
+
+(define (TargetNumberCheck [argument #f])
+ ;Now we write our condition for the stack:
+  (cond
+;number? means is the argument, within the reader script that is being passed, a number?
+;If the argument IS, in fact, a number then push this argument onto the stack.
+    [(positive? argument) (display argument)]
+;Or, is the argument being passed within the reader equal to a * + / or - operand?
+    [(or (equal? TargetNumber argument))
+;define 'op-result', whereby, two arguments already on the stack are popped off and evaluated with the operand passed
+     (define target-result (argument))
+;push the result of the evaluation of the two elements popped off with the operand onto the stack
+     (target-result)]))
 ;make the 'handle' function available ti the 'reader' script
 (provide handle)
+(provide TargetNumber)
 
 ;Also, make the opeands available to the reader script
 (provide + * / -)
-
+(provide RandomListFirstNumber)
+(provide RandomListSecondNumber)
+(provide RandomListThirdNumber)
+(provide RandomListFourthNumber)
+(provide RandomListFifthNumber)
+(provide RandomListSixthNumber)
